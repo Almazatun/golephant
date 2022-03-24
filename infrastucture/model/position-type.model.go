cb3c9f97eb3c188cddb6718f187e2ae1c19ef1b4
Almazatun <atunstyle@gmail.com>
2022-03-24 14:58:49 +0600

    Rename folder

--- infrastucture/model/position-type.model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-import "github.com/google/uuid"
-
-type PositionType struct {
-	Id    uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	Title string    `json:"title" validate:"required" gorm:"type:varchar(100); not NULL; unique"`
-}
