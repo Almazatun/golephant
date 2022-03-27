2e425a741b3db4f07b905ca0cda8bcc29b5e19d5
Almazatun <atunstyle@gmail.com>
2022-03-27 20:51:22 +0600

    Add Creat, FindById, Update

--- infrastucture/user.repository.go
+++ infrastucture/user.repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/jinzhu/gorm"
 
@@ -14,22 +15,20 @@ type userRepository struct {
 }
 
 type UserRepo interface {
-	Save(user *entity.User) (registerUser *entity.User, err error)
+	Create(user entity.User) (registerUser *entity.User, err error)
 	FindByEmail(email string) (user *entity.User, err error)
+	FindById(userId string) (user *entity.User, err error)
+	Update(updateUserData entity.User) (updateUser *entity.User, err error)
 }
 
 func NewUserRepo(db *gorm.DB) UserRepo {
 	return &userRepository{db: db}
 }
 
-func (r *userRepository) Save(user *entity.User) (registerUser *entity.User, err error) {
-	createUser := &entity.User{
-		Username: user.Username,
-		Email:    user.Email,
-		Password: user.Password,
-	}
+func (r *userRepository) Create(user entity.User) (registerUser *entity.User, err error) {
+	var createUser entity.User
 
-	result := r.db.Create(&createUser)
+	result := r.db.Create(&user)
 
 	er := result.Error
 
@@ -37,13 +36,17 @@ func (r *userRepository) Save(user *entity.User) (registerUser *entity.User, err
 		return nil, err
 	}
 
-	return createUser, nil
+	createUser = user
+
+	return &createUser, nil
 }
 
 func (r *userRepository) FindByEmail(email string) (userDB *entity.User, err error) {
 	var user entity.User
 
 	result := r.db.First(&user, "email = ?", email)
+
+	fmt.Println(user)
 
 	dbErr := result.Error
 
@@ -54,4 +57,35 @@ func (r *userRepository) FindByEmail(email string) (userDB *entity.User, err err
 	}
 
 	return &user, nil
+}
+
+func (r *userRepository) FindById(userId string) (userDB *entity.User, err error) {
+	var user entity.User
+
+	result := r.db.First(&user, "user_id = ?", userId)
+
+	dbErr := result.Error
+
+	if dbErr != nil {
+		err := errors.New(error_message.USER_NOT_FOUND)
+
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *userRepository) Update(updateUserData entity.User) (updateUser *entity.User, err error) {
+	var user entity.User
+
+	result := r.db.Model(&user).Updates(updateUserData)
+
+	e := result.Error
+
+	if e != nil {
+		return nil, e
+	}
+
+	return &user, nil
+
 }
