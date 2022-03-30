0ed52462b8f9262d5942c22fa80eecf0b79fb6b7
Almazatun <atunstyle@gmail.com>
2022-03-31 00:17:01 +0600

    Add user-education, user-experience vars

--- common/error-message/error-message.go
+++ common/error-message/error-message.go
@@ -1,7 +1,9 @@
 package error_message
 
-var (
-	UNAUTHORIZED       = "Unauthorized"
-	INCCORECT_PASSWORD = "Incorrect password"
-	USER_NOT_FOUND     = "User not found"
+const (
+	UNAUTHORIZED              = "Unauthorized"
+	INCCORECT_PASSWORD        = "Incorrect password"
+	USER_NOT_FOUND            = "User not found"
+	USER_EDUCATION_NOT_FOUND  = "User education not found"
+	USER_EXPERIENCE_NOT_FOUND = "User experience not found"
 )
