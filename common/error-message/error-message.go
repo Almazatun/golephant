9a3ef6e3732adf73e16ca2cfa114401cf23dbff7
Almazatun <atunstyle@gmail.com>
2022-04-01 11:32:48 +0600

    Add bad request

--- common/error-message/error-message.go
+++ common/error-message/error-message.go
@@ -6,4 +6,5 @@ const (
 	USER_NOT_FOUND            = "User not found"
 	USER_EDUCATION_NOT_FOUND  = "User education not found"
 	USER_EXPERIENCE_NOT_FOUND = "User experience not found"
+	BAD_REGUEST               = "Bad request"
 )
