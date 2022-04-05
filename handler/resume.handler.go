758b1235e2cf1517b04f8d310c2d0294a03679f8
Almazatun <atunstyle@gmail.com>
2022-04-05 18:39:51 +0600

    Add new resume handles

--- handler/resume.handler.go
+++ handler/resume.handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	loggerinfo "github.com/Almazatun/golephant/common/loggerInfo"
@@ -16,6 +17,13 @@ type resumeHandler struct {
 
 type ResumeHandler interface {
 	CreateResume(w http.ResponseWriter, r *http.Request)
+	UpdateBasicInfoResume(w http.ResponseWriter, r *http.Request)
+	UpdateAboutMeResume(w http.ResponseWriter, r *http.Request)
+	UpdateCitizenshipResume(w http.ResponseWriter, r *http.Request)
+	UpdateTagsResumeInput(w http.ResponseWriter, r *http.Request)
+	UpdateUserEducationResume(w http.ResponseWriter, r *http.Request)
+	UpdateUserExperiencesResume(w http.ResponseWriter, r *http.Request)
+	UpdateDesiredPositionResume(w http.ResponseWriter, r *http.Request)
 	DeleteResume(w http.ResponseWriter, r *http.Request)
 	DeleteUserEducationInResume(w http.ResponseWriter, r *http.Request)
 	DeleteUserExperienceInResume(w http.ResponseWriter, r *http.Request)
@@ -38,7 +46,7 @@ func (h *resumeHandler) CreateResume(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	user, err := h.resumeUseCase.CreateResume(params["userId"], createResumeInput)
+	res, err := h.resumeUseCase.CreateResume(params["userId"], createResumeInput)
 
 	if err != nil {
 		loggerinfo.LoggerError(err)
@@ -47,7 +55,177 @@ func (h *resumeHandler) CreateResume(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (h *resumeHandler) UpdateBasicInfoResume(w http.ResponseWriter, r *http.Request) {
+	var updateBasicInfoResumeInput input.UpdateBasicInfoResume
+	params := mux.Vars(r)
+
+	err := json.NewDecoder(r.Body).Decode(&updateBasicInfoResumeInput)
+
+	fmt.Println(updateBasicInfoResumeInput)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.resumeUseCase.UpdateBasicInfoResume(params["userId"], params["resumeId"], updateBasicInfoResumeInput)
+
+	if err != nil {
+		loggerinfo.LoggerError(err)
+		HttpResponseBody(w, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func (h *resumeHandler) UpdateAboutMeResume(w http.ResponseWriter, r *http.Request) {
+	var updateAboutMeResumeInput input.UpdateAboutMeResumeInput
+	params := mux.Vars(r)
+
+	err := json.NewDecoder(r.Body).Decode(&updateAboutMeResumeInput)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.resumeUseCase.UpdateAboutMeResume(params["userId"], params["resumeId"], updateAboutMeResumeInput)
+
+	if err != nil {
+		loggerinfo.LoggerError(err)
+		HttpResponseBody(w, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func (h *resumeHandler) UpdateCitizenshipResume(w http.ResponseWriter, r *http.Request) {
+	var updateCitizenshipResumeInput input.UpdateCitizenshipResumeInput
+	params := mux.Vars(r)
+
+	err := json.NewDecoder(r.Body).Decode(&updateCitizenshipResumeInput)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.resumeUseCase.UpdateCitizenshipResume(params["userId"], params["resumeId"], updateCitizenshipResumeInput)
+
+	if err != nil {
+		loggerinfo.LoggerError(err)
+		HttpResponseBody(w, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func (h *resumeHandler) UpdateTagsResumeInput(w http.ResponseWriter, r *http.Request) {
+	var udateTagsResumeInput input.UdateTagsResumeInput
+	params := mux.Vars(r)
+
+	err := json.NewDecoder(r.Body).Decode(&udateTagsResumeInput)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.resumeUseCase.UpdateTagsResumeInput(params["userId"], params["resumeId"], udateTagsResumeInput)
+
+	if err != nil {
+		loggerinfo.LoggerError(err)
+		HttpResponseBody(w, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func (h *resumeHandler) UpdateUserEducationResume(w http.ResponseWriter, r *http.Request) {
+	var updateUserEducationsResumeInput input.UpdateUserEducationsResumeInput
+	params := mux.Vars(r)
+
+	err := json.NewDecoder(r.Body).Decode(&updateUserEducationsResumeInput)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.resumeUseCase.UpdateUserEducationResume(params["userId"], params["resumeId"], updateUserEducationsResumeInput)
+
+	if err != nil {
+		loggerinfo.LoggerError(err)
+		HttpResponseBody(w, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func (h *resumeHandler) UpdateUserExperiencesResume(w http.ResponseWriter, r *http.Request) {
+	var updateUserExperiencesResumeInput input.UpdateUserExperiencesResumeInput
+	params := mux.Vars(r)
+
+	err := json.NewDecoder(r.Body).Decode(&updateUserExperiencesResumeInput)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.resumeUseCase.UpdateUserExperiencesResume(params["userId"], params["resumeId"], updateUserExperiencesResumeInput)
+
+	if err != nil {
+		loggerinfo.LoggerError(err)
+		HttpResponseBody(w, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func (h *resumeHandler) UpdateDesiredPositionResume(w http.ResponseWriter, r *http.Request) {
+	var updateDesiredPositionResumeInput input.UpdateDesiredPositionResumeInput
+	params := mux.Vars(r)
+
+	err := json.NewDecoder(r.Body).Decode(&updateDesiredPositionResumeInput)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.resumeUseCase.UpdateDesiredPositionResume(params["userId"], params["resumeId"], updateDesiredPositionResumeInput)
+
+	if err != nil {
+		loggerinfo.LoggerError(err)
+		HttpResponseBody(w, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(res)
+
 }
 
 func (h *resumeHandler) DeleteResume(w http.ResponseWriter, r *http.Request) {
