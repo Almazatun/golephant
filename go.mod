3c05b901410a0f3d8364580e447f637b53108ee7
Almazatun <atunstyle@gmail.com>
2022-04-10 00:20:21 +0600

    Add test dir

--- go.mod
+++ go.mod
@@ -3,17 +3,23 @@ module github.com/Almazatun/golephant
 go 1.13
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.4.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.2
+	github.com/onsi/ginkgo v1.16.5
+	github.com/onsi/gomega v1.19.0
+	github.com/stretchr/testify v1.7.1 // indirect
 	golang.org/x/crypto v0.0.0-20220208233918-bba287dce954
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
---
+++ test/user.repository_test.go
@@ -0,0 +1,71 @@
+package test
+
+import (
+	"database/sql"
+	"regexp"
+	"time"
+
+	repository "github.com/Almazatun/golephant/infrastucture"
+	"github.com/Almazatun/golephant/infrastucture/entity"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Pg", func() {
+	var repo repository.UserRepo
+	var mock sqlmock.Sqlmock
+
+	BeforeEach(func() {
+		var db *sql.DB
+		var err error
+
+		db, mock, err = sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherRegexp)) // mock sql.DB
+		Expect(err).ShouldNot(HaveOccurred())
+
+		gdb, err := gorm.Open("postgres", db) // open gorm db
+		Expect(err).ShouldNot(HaveOccurred())
+
+		repo = repository.NewUserRepo(gdb)
+	})
+
+	Context("Register", func() {
+		var userDB entity.User
+
+		rows := sqlmock.NewRows([]string{
+			"username", "email", "password",
+		}).AddRow(userDB.Username, userDB.Email, userDB.Password)
+
+		BeforeEach(func() {
+			userDB = entity.User{
+				Email:        "test@mail.com",
+				Password:     "1234567",
+				Username:     "test",
+				CreationTime: time.Now(),
+				UpdateTime:   time.Now(),
+			}
+		})
+
+		It("Create", func() {
+			mock.ExpectBegin()
+
+			const sqlInsert = `
+					INSERT INTO "users" ("username","email","password")
+					VALUES ($1,$2,$3) RETURNING "users"."user_id"`
+			const user_id = 1
+			mock.ExpectQuery(regexp.QuoteMeta(sqlInsert)).
+				WithArgs(userDB.UserID, userDB.Email, userDB.Password).
+				WillReturnRows(rows)
+			mock.ExpectCommit() // commit transaction
+			Expect(userDB.Email).Should(Equal("test@mail.com"))
+
+			_, err := repo.Create(userDB)
+			Expect(err).ShouldNot(HaveOccurred())
+
+		})
+
+	})
+
+})
