70427d733b3bad98eb24c11df9e716fc14dfcec0
Almazatun <atunstyle@gmail.com>
2022-04-12 19:52:12 +0600

    Delete validation option from colums

--- infrastucture/entity/company.entity.go
+++ infrastucture/entity/company.entity.go
@@ -8,9 +8,10 @@ import (
 
 type Company struct {
 	CompanyID      uuid.UUID        `json:"company_id" gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Title          string           `json:"title" validate:"required" gorm:"type:varchar(100); not NULL; unique"`
-	Email          string           `json:"email" validate:"required,email,omitempty" gorm:"type:varchar(100); not NULL;"`
-	Phone          string           `json:"phone" validate:"required" gorm:"type:varchar(100); not NULL;"`
+	Title          string           `json:"title" gorm:"type:varchar(100); not NULL; unique"`
+	Email          string           `json:"email" gorm:"type:varchar(100); not NULL; unique"`
+	Password       string           `json:"password" gorm:"type:varchar(100); not NULL"`
+	Phone          string           `json:"phone" gorm:"type:varchar(100);"`
 	CreationTime   time.Time        `json:"creation_time" gorm:"type:date; not NULL;"`
 	UpdateTime     time.Time        `json:"update_time" gorm:"type:date; not NULL;"`
 	CompanyAddress []CompanyAddress `gorm:"ForeignKey:CompanyID;references:CompanyID;"`
