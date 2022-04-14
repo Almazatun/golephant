a6bb04e523b27720d32eb38a81be9000ae229012
Almazatun <atunstyle@gmail.com>
2022-04-14 23:20:36 +0600

    Delete position-type entity and include new field in position

--- infrastucture/entity/position-type.entity.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-import "github.com/google/uuid"
-
-type PositionType struct {
-	PositionTypeID uuid.UUID `json:"position_type_id" gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Title          string    `json:"title" validate:"required" gorm:"type:varchar(100); not NULL; unique"`
-}
--- infrastucture/entity/position.entity.go
+++ infrastucture/entity/position.entity.go
@@ -8,9 +8,10 @@ import (
 type Position struct {
 	PositionID       uuid.UUID      `json:"position_id" gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
 	Description      string         `json:"name" gorm:"type:varchar(1000)"`
-	Requirements     pq.StringArray `json:"requirements" gorm:"type:text[]" validate:"required"`
-	Responsibilities pq.StringArray `json:"responsibilities" gorm:"type:text[]" validate:"required"`
-	PositionType     PositionType   `json:"position_type"  gorm:"type:text"`
+	Requirements     pq.StringArray `json:"requirements" gorm:"type:text[]"`
+	Responsibilities pq.StringArray `json:"responsibilities" gorm:"type:text[]"`
+	PositionType     string         `json:"position_type"  gorm:"type:text"`
+	Salary           int            `json:"salary" gorm:"type:int"`
 	CompanyID        uuid.UUID      `json:"company_id" gorm:"OnDelete:SET NULL; not NULL"`
 	Company          Company
 }
