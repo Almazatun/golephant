3def605db5f1b0d7b065fd3d7893af656a2d7476
Almazatun <atunstyle@gmail.com>
2022-04-26 00:06:01 +0600

    Update CORS_ALLOWED_HEADERS

--- pkg/http/cors/cors-vars.go
+++ pkg/http/cors/cors-vars.go
@@ -1,7 +1,7 @@
 package cors
 
 var (
-	CORS_ALLOWED_HEADERS = []string{"*"}
+	CORS_ALLOWED_HEADERS = []string{"*", "X-Requested-With", "Content-Type", "Authorization"}
 	CORS_ALLOWED_METHODS = []string{"GET", "POST", "PUT", "DELETE"}
 	CORS_ALLOWED_ORIGINS = []string{"*"}
 )
